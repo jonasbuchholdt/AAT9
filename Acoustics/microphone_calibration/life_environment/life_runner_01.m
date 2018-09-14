aaf829091892fe98130a07509f30c754453cbb5c
ckirsc17 <ckirsc17@student.aau.dk>
2018-09-14 12:58:10 +0200

    update saucard

--- Acoustics/microphone_calibration/life_environment/life_runner_01.m
+++ Acoustics/microphone_calibration/life_environment/life_runner_01.m
@@ -1,13 +1,16 @@
 clear all
 
-filename = 'testlog_01.xls';
+filename = 'testlog_01.xlsx';
 fs = 48000;                                 % sample rate       [Hz]
 blength = 3;                                % buffer length      [s]
+tablelines = 8;                             % number of lines that are
+                                            % buffered before writing file
+TL = 0;
+B= 0;
+B=logical(B);
 
-soundcard = audioDeviceReader(fs);          % setting up audio object
-%soundcard.Driver('ASIO')                    % choosing audio driver
-soundcard.SamplesPerFrame = fs/8;           % setting up framesize for
-                                            % fast sound weighting
+soundcard = audioDeviceReader('Driver','ASIO','SampleRate',fs,'SamplesPerFrame',2048);          % setting up audio object
+
 
 buffer = zeros(blength * fs, 1);            % initializing audio buffer
 
@@ -21,12 +24,24 @@ tic;
 
 while toc < 10
     audioin = soundcard();                  % fetch samples from soundcard
-    buffer = [buffer(6001:end); audioin];   % update buffer
-    RMS = dummy(buffer);
-    logtable = [logtable; table(RMS)]
+    buffer = [buffer(2049:end); audioin];   % update buffer
+    if B
+        RMS = dummy(buffer);
+        logtable = [logtable; table(RMS)];
+        TL = TL + 1;
+    end
+    B= ~ B;
+    
+    if TL >= tablelines
+%        writetable(logtable,filename);
+        RMS=[];
+        logtable = table(RMS);
+        TL = 0;
+    end
 end
 
-
+close all
+plot(buffer)
 
 
 
