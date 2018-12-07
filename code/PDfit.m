953e4b928c8bfe5722157b75e8f290e130919a3c
Christoph Kirsch <ckirsc17@student.aau.dk>
2018-12-07 16:29:49 +0100

    last commit on fridaaaaaay

---
+++ code/PDfit.m
@@ -0,0 +1,20 @@
+clearvars
+close all
+
+
+N = 100000;                                 % number of samples
+
+x = [-5:0.1:5];
+
+%samples = rand(1,N);
+samples = randn(1,N);
+
+my = mean(samples)
+ssq= var(samples);
+
+gaussian = (1/sqrt(2*pi*ssq))*exp(-((x-my).^2)/(2*ssq));
+
+figure()
+histogram(samples, 25,'Normalization','probability')
+hold on
+plot(x,gaussian)
