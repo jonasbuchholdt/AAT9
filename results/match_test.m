e0f23fe5e5bf7d9a2ba52efa5a0b29cf7e55de09
jonas buchholdt <jbuchh13@student.aau.dk>
2018-12-13 09:51:41 +0100

    cdf

--- results/match_test.m
+++ results/match_test.m
@@ -22,16 +22,51 @@ S8907521 = [50 48 50 47];
 S8627107 = [45 47 40 47];
 S9823032 = [50 47 47 47 51 48];
 
-means = round([mean(S1208596) mean(S4842965) mean(S5800904) mean(S5814465(2:end))...
-    mean([S6259598(1:2) S6259598(4)]) mean(S7169829) mean(S7690291(1:3)) mean(S8907521(1:3))...
+meansf = round([mean(S1208596f) mean(S4842965f) mean(S5800904f) mean(S5814465f)...
+    mean(S6259598f) mean(S7169829f) mean(S7690291f) mean(S8907521f)...
+    mean(S8627107f) mean(S9823032f)]);
+
+
+%meansfc = round([mean(S1208596) mean(S4842965) mean(S5800904) mean(S5814465(2:end))...
+ %   mean([S6259598(1:2) S6259598(4)]) mean(S7169829) mean(S7690291) mean(S8907521)...
+  %  mean([S8627107(1:2) S8627107(4)]) mean(S9823032)]);
+
+
+meansrc = round([mean(S1208596) mean(S4842965) mean(S5800904) mean(S5814465)...
+    mean(S6259598) mean(S7169829) mean(S7690291) mean(S8907521)...
     mean([S8627107(1:2) S8627107(4)]) mean(S9823032)]);
 
-
-means_w_fpositive = round([mean(S1208596) mean(S4842965) mean(S5800904) mean(S5814465)...
+meansnc = round([mean(S1208596) mean(S4842965) mean(S5800904) mean(S5814465)...
     mean(S6259598) mean(S7169829) mean(S7690291) mean(S8907521)...
     mean(S8627107) mean(S9823032)]);
 
-means_used = [51 51 48 48 43 52 46 49 46 48]
+meansd = meansrc-meansf;
+
+means_used = [48 48 48 48 48 48 48 48 48 48];
+means_used = [51 51 48 48 43 52 46 49 46 48];
+
+
+S1208596srt = [mean([11 8.4])+means_used(1)-48 11];
+S4842965srt = [mean([21 22])+means_used(2)-48 mean([13 10])];
+S5800904srt = [mean([2.9 -0.61])+means_used(3)-48 mean([-2.7 -1.8])];
+S5814465srt = [mean([-7.8 -6.6])+means_used(4)-48 mean([-2.6 -5.1])];
+S6259598srt = [mean([18 17])+means_used(5)-48 mean([4.3 5.2])];
+S7169829srt = [mean([1.4 0.094])+means_used(6)-48 mean([3.1 2.4])];
+S7690291srt = [mean([9.7 13])+means_used(7)-48 mean([1.2 -0.47])];
+S8907521srt = [mean([-1.3 0.094])+means_used(8)-48 mean([-1.5 -2.5])];
+S8627107srt = [mean([-2.5 0.33])+means_used(9)-48 mean([-7.3 -6.9])];
+S9823032srt = [mean([4 -0.75])+means_used(10)-48 mean([2.8 -0.75])];
+
+%bone
+match(1,:) = [S1208596srt(1) S4842965srt(1) S5800904srt(1) S5814465srt(1) S6259598srt(1)...
+    S7169829srt(1) S7690291srt(1) S8907521srt(1) S8627107srt(1) S9823032srt(1)];
+
+%air
+match(2,:) = [S1208596srt(2) S4842965srt(2) S5800904srt(2) S5814465srt(2) S6259598srt(2)...
+    S7169829srt(2) S7690291srt(2) S8907521srt(2) S8627107srt(2) S9823032srt(2)];
+
+mean(match,2)
+std(match')'
 %%
 
 init = [48 50.7 52 46 49 51 50 53.3];
